52aa87d2fe00939856f83eb3e335625071d1d6f5
Ruan <ruangamer10@hotmail.com>
2019-08-22 16:47:27 -0300

    Codigo da tela de Consultas

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase:  {
+    apiKey: "AIzaSyBPMMBvU3IN7Qg6B21d4zc3CHxeT3lmyp8",
+    authDomain: "consultas-75db5.firebaseapp.com",
+    databaseURL: "https://consultas-75db5.firebaseio.com",
+    projectId: "consultas-75db5",
+    storageBucket: "",
+    messagingSenderId: "951143256884",
+    appId: "1:951143256884:web:ef4da3da8f478af9"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
