ed92e80e05be4f8326621bcc825a6ca9fa3192c6
Ruan <ruangamer10@hotmail.com>
2019-08-22 17:14:53 -0300

    mudança no html

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,30 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor( private camera: Camera
+    ) { }
 
+  ngOnInit() { }
+
+  async openGalery() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      correctOrientation: true
+    };
+    try {
+      const fileUri: string = await this.camera.getPicture(options);
+
+      let file: string;
+      if (this.plataform.is('ios')){
+        file = fileUri.split('/').pop();
+      }else {
+        file = fileUri.substring(fileUri.lastIndexOf('/') + 1, fileUri.indexOf('?'));
+      }
+        const path: string = fileUri.substring(0, fileUri.lastIndexOf('/'));
+      } catch(error){
+        console.error(error);
+    }
+  }
 }
